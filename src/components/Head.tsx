a2f5d7c76cb8c67377f267c7281a0dbedba4aae4
tolgaand <tolgaand@yandex.com>
2022-10-25 01:46:40 +0300

    Update meta description and keywords

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,9 +6,12 @@ export const Head = () => {
       <title>Aptos Faucet</title>
       <meta
         name="description"
-        content="Send apt to your account on the devnet network."
+        content="Send apt to a wallet quickly and securely on the devnet or testnet network."
       />
-      <meta name="keywords" content="Aptos, Apt, Faucet" />
+      <meta
+        name="keywords"
+        content="Aptos, Apt, Faucet, Aptos faucet, aptos faucet devnet, aptos faucet testnet"
+      />
       <link rel="icon" href="/favicon.ico" />
     </_Head>
   );
