39d02ddf5d540a75cb65753624833b842a313ca3
tolgaand <tolgaand@yandex.com>
2022-10-25 20:50:18 +0300

    Add logo and testnet button

--- src/components/AptFaucet.tsx
+++ src/components/AptFaucet.tsx
@@ -1,7 +1,8 @@
-import { AptosClient, AptosAccount, CoinClient, FaucetClient } from "aptos";
+import { FaucetClient } from "aptos";
 import {
   Box,
   Button,
+  Image,
   Input,
   Modal,
   ModalBody,
@@ -14,9 +15,13 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { GithubIcon } from "./GithubIcon";
 
-const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
-const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
+const NODE_URL_DEVNET = "https://fullnode.devnet.aptoslabs.com/v1";
+const FAUCET_URL_DEVNET = "https://faucet.devnet.aptoslabs.com";
+
+const NODE_URL_TESTNET = "https://fullnode.testnet.aptoslabs.com/v1";
+const FAUCET_URL_TESTNET = "https://faucet.testnet.aptoslabs.com";
 
 export const AptFaucet = () => {
   const [walletAddress, setWalletAddress] = useState("");
@@ -24,9 +29,13 @@ export const AptFaucet = () => {
   const [modalTitle, setModalTitle] = useState("");
   const [modalMessage, setModalMessage] = useState("");
 
-  const sendApt = async () => {
+  const sendApt = async (type: "devnet" | "testnet") => {
+    if (!walletAddress) return;
+
     try {
-      if (!walletAddress) return;
+      const NODE_URL = type === "devnet" ? NODE_URL_DEVNET : NODE_URL_TESTNET;
+      const FAUCET_URL =
+        type === "devnet" ? FAUCET_URL_DEVNET : FAUCET_URL_TESTNET;
 
       const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
 
@@ -50,28 +59,58 @@ export const AptFaucet = () => {
       alignItems="center"
       height="100vh"
       margin="auto"
-      maxW="2xl"
+      maxW="5xl"
     >
-      <Text fontSize="4xl">APT Faucet🚀</Text>
-      <Text fontSize="15px">Send $APT to a wallet on the DEVNET network.</Text>
-      <Stack marginTop="20px">
+      <Image src="/logo.png" height="150px" />
+      <Text fontSize="1xl" marginTop="15px" fontWeight="bold" color="#F17844">
+        Quickly send a $APT to your wallet.
+      </Text>
+      <Stack marginTop="20px" maxWidth="2xl" width="100%">
         <Input
           variant="filled"
           placeholder="0x..."
           value={walletAddress}
           onChange={(e) => setWalletAddress(e.target.value)}
         />
-        <Button
-          backgroundColor="rgb(6, 247, 247)"
-          color="rgb(42, 49, 49)"
-          _hover={{
-            backgroundColor: "rgb(6, 247, 247)",
-          }}
-          onClick={sendApt}
-        >
-          Send 1 APT!
-        </Button>
+        <Stack direction="row">
+          <Button
+            backgroundColor="rgb(6, 247, 247)"
+            color="rgb(42, 49, 49)"
+            _hover={{
+              backgroundColor: "rgb(6, 247, 247)",
+            }}
+            onClick={() => sendApt("devnet")}
+            flex="1"
+            textTransform="uppercase"
+            fontWeight="bold"
+          >
+            Send Devnet
+          </Button>
+          <Button
+            backgroundColor="rgb(6, 247, 247)"
+            color="rgb(42, 49, 49)"
+            _hover={{
+              backgroundColor: "rgb(6, 247, 247)",
+            }}
+            onClick={() => sendApt("testnet")}
+            flex="1"
+            textTransform="uppercase"
+            fontWeight="bold"
+          >
+            Send Testnet
+          </Button>
+        </Stack>
       </Stack>
+
+      <Box marginTop="10px">
+        <a
+          href="https://github.com/tolgaand/aptosfaucet"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GithubIcon />
+        </a>
+      </Box>
 
       <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
         <ModalOverlay />
