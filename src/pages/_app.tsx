92add1a4565d460241fdfdb3a04f401a7efb2519
tolgaand <tolgaand@yandex.com>
2022-10-30 19:35:53 +0300

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../assets/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import theme from "../theme";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <GoogleAnalytics trackPageViews />
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
