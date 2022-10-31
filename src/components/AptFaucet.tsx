21c90de849285d4a2f55c5a39a9d13c9f240f0fc
tolgaand <tolgaand@yandex.com>
2022-10-31 17:30:38 +0300

    Add input for apt amount and loading to buttons

--- src/components/AptFaucet.tsx
+++ src/components/AptFaucet.tsx
@@ -11,8 +11,14 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
   Stack,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { GithubIcon } from "./GithubIcon";
@@ -23,31 +29,47 @@ const FAUCET_URL_DEVNET = "https://faucet.devnet.aptoslabs.com";
 const NODE_URL_TESTNET = "https://fullnode.testnet.aptoslabs.com/v1";
 const FAUCET_URL_TESTNET = "https://faucet.testnet.aptoslabs.com";
 
+const sanitizeAddress = (address: string) => {
+  return address.slice(0, 4) + "..." + address.slice(-4);
+};
+
 export const AptFaucet = () => {
   const [walletAddress, setWalletAddress] = useState("");
-  const [showModal, setShowModal] = useState(false);
-  const [modalTitle, setModalTitle] = useState("");
-  const [modalMessage, setModalMessage] = useState("");
+  const [amount, setAmount] = useState("1");
+
+  //loading
+  const [loading, setLoading] = useState(false);
+
+  const toast = useToast();
 
   const sendApt = async (type: "devnet" | "testnet") => {
     if (!walletAddress) return;
 
     try {
+      setLoading(true);
       const NODE_URL = type === "devnet" ? NODE_URL_DEVNET : NODE_URL_TESTNET;
       const FAUCET_URL =
         type === "devnet" ? FAUCET_URL_DEVNET : FAUCET_URL_TESTNET;
 
       const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
 
-      await faucetClient.fundAccount(walletAddress, 100_000_000);
-      setModalTitle("Success");
-      setModalMessage("Aptos sent to your wallet!");
-    } catch (error) {
-      console.log(error);
-      setModalTitle("Error");
-      setModalMessage(JSON.stringify(error));
+      await faucetClient.fundAccount(walletAddress, 1e9 * Number(amount));
+
+      toast({
+        title: "Success",
+        description: `${amount} APT sent to ${sanitizeAddress(walletAddress)}`,
+        status: "success",
+        duration: 9000,
+      });
+    } catch (e: any) {
+      toast({
+        title: "Error",
+        description: JSON.stringify(e.message, null, 2),
+        status: "error",
+        duration: 9000,
+      });
     } finally {
-      setShowModal(true);
+      setLoading(false);
     }
   };
 
@@ -83,9 +105,20 @@ export const AptFaucet = () => {
             flex="1"
             textTransform="uppercase"
             fontWeight="bold"
+            isLoading={loading}
           >
             Send Devnet
           </Button>
+          <NumberInput maxW={20} defaultValue={1} min={1}>
+            <NumberInputField
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
           <Button
             backgroundColor="rgb(6, 247, 247)"
             color="rgb(42, 49, 49)"
@@ -96,6 +129,7 @@ export const AptFaucet = () => {
             flex="1"
             textTransform="uppercase"
             fontWeight="bold"
+            isLoading={loading}
           >
             Send Testnet
           </Button>
@@ -111,15 +145,6 @@ export const AptFaucet = () => {
           <GithubIcon />
         </a>
       </Box>
-
-      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{modalTitle}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>{modalMessage}</ModalBody>
-        </ModalContent>
-      </Modal>
     </Box>
   );
 };
