3fe60e744f3aaedbf62af0bc991fee615b1f7f70
tolgaand <tolgaand@yandex.com>
2023-01-18 03:17:22 +0300

    feat: create the send aptos hook

---
+++ src/hooks/useSendAptos.ts
@@ -0,0 +1,58 @@
+import { useToast } from "@chakra-ui/react";
+import { FaucetClient } from "aptos";
+import { sanitizeAddress } from "lib/sanitizeAddress";
+import { useMutation } from "react-query";
+
+const NODE_URL_DEVNET = "https://fullnode.devnet.aptoslabs.com/v1";
+const FAUCET_URL_DEVNET = "https://faucet.devnet.aptoslabs.com";
+
+const NODE_URL_TESTNET = "https://fullnode.testnet.aptoslabs.com/v1";
+const FAUCET_URL_TESTNET = "https://faucet.testnet.aptoslabs.com";
+
+const networkWithFaucet = {
+  devnet: {
+    NODE_URL: NODE_URL_DEVNET,
+    FAUCET_URL: FAUCET_URL_DEVNET,
+  },
+  testnet: {
+    NODE_URL: NODE_URL_TESTNET,
+    FAUCET_URL: FAUCET_URL_TESTNET,
+  },
+};
+
+export const useSendAptos = (
+  walletAddress: string,
+  network: "devnet" | "testnet"
+) => {
+  const toast = useToast();
+
+  return useMutation(
+    ["sendAptos", walletAddress, network],
+    async () => {
+      const NODE_URL = networkWithFaucet[network].NODE_URL;
+      const FAUCET_URL = networkWithFaucet[network].FAUCET_URL;
+
+      const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
+
+      return await faucetClient.fundAccount(walletAddress, 1e9 * Number(1));
+    },
+    {
+      onSuccess: () => {
+        toast({
+          title: "Success",
+          description: `APT sent to ${sanitizeAddress(walletAddress)}`,
+          status: "success",
+          duration: 9000,
+        });
+      },
+      onError: (error: any) => {
+        toast({
+          title: "Error",
+          description: JSON.stringify(error.message, null, 2),
+          status: "error",
+          duration: 9000,
+        });
+      },
+    }
+  );
+};
